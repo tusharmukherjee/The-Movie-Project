39a8651a44b139154638ee643572159ab5f532a6
Tushar Mukherjee <tushar1214mrt@gmail.com>
2022-07-19 18:40:01 +0530

    correct name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorkeRegistration from "./serviceWorkerRegistration";
+import * as serviceWorkeRegistration from "./serviceWorkeRegistration";
 
 ReactDOM.render(
   <React.StrictMode>
